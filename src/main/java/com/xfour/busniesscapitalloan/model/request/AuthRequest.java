2eca4d97d21b88b217e286b6402f4dd47006f144
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-23 16:57:49 +0700

    Auth Request-Response

---
+++ src/main/java/com/xfour/busniesscapitalloan/model/request/AuthRequest.java
@@ -0,0 +1,25 @@
+package com.xfour.busniesscapitalloan.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    @Email(message = "email is invalid")
+    @NotBlank(message = "email is required")
+    private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "password must have at least 8 characters")
+    private String password;
+}
---
+++ src/main/java/com/xfour/busniesscapitalloan/model/response/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.xfour.busniesscapitalloan.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+    private List<String> roles;
+}
